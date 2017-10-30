c34d9d666be7346c986d690c463cfb13e3518830
k38875 <christian.neuerer@student.fhws.de>
2017-10-30 14:47:12 +0100

    refactored

--- main.cpp
+++ main.cpp
@@ -17,30 +17,42 @@ bool isPerfect(int zahl, int array[]) {
     return zahl2 == zahl;
 }
 
-int main() {
-    int i = 0;
-    auto time = std::chrono::system_clock::now();
-    auto time2 = std::time(nullptr);
+void ausgabe(int array[], int j, int i) {
+    std::cout << "perfekte zahl: " << i << " teiler: ";
     do {
-        int array[500] = {0};
-        int j = 0;
-        if (isPerfect(i, array)) {
-            std::cout << "perfekte zahl: " << i << " teiler: ";
-            do{
-                std::cout << array[j] << " ";
-                j++;
-            }while(array[j] != 0);
-            std::cout << std::endl;
-            time = std::chrono::system_clock::now();
-            time2 = std::time(nullptr);
-        }
-        i++;
-        auto after = std::chrono::system_clock::now();
+        std::cout << array[j] << " ";
+        j++;
+    } while (array[j] != 0);
+    std::cout << std::endl;
+}
+
+void berechnung(int *i, std::time_t *time2) {
+    int array[500] = {0};
+    int j = 0;
+    if (isPerfect(*i, array)) {
+        ausgabe(array, j, *i);
+        *time2 = std::time(nullptr);
+    }
+    (*i)++;
+}
+
+void TimeCheckLoop(std::time_t time2, int* pI) {
+    do {
+        std::time_t *pTime2 = &time2;
+        berechnung(pI, pTime2);
         auto after2 = std::time(nullptr);
         if ((after2 - time2) >= 120) {
             break;
         }
     } while (true);
+}
+
+int main() {
+    int i = 0;
+    int* pI = &i;
+    auto time2 = std::time(nullptr);
+    TimeCheckLoop(time2, pI);
+    std::cout << "Letzte Zahl: " << i;
     //std::cin.get();
     return 0;
 }
